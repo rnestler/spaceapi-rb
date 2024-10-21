f4bf0599f07cde800bb76d494e1f4b1ab049fb16
Raphael Nestler <raphael.nestler@renuo.ch>
2024-10-21 16:10:49 +0200

    Initial scaffolded gem

---
+++ lib/spaceapi.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "spaceapi/version"
+require_relative "spaceapi/spaceapi"
+
+module Spaceapi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/spaceapi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Spaceapi
+  VERSION = "0.1.0"
+end
